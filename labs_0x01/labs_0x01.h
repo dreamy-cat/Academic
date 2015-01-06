0b9e5bad52e0d657f20838ec7f65fff5e1bad930
Dreamy Cat <funny.dreamy.cat@gmail.com>
2015-01-06 19:32:36 +0300

    All tasks from chapter 2. Organized directories.

---
+++ labs_0x01/labs_0x01.h
@@ -0,0 +1,10 @@
+#ifndef LABS_0x01_H
+#define LABS_0x01_H
+
+#include "chapter02.h"
+
+void Labs_0x01();
+
+void chapter_02();
+
+#endif
--- main.cpp
+++ main.cpp
@@ -3,34 +3,13 @@
 #include <fstream>
 #include <vector>
 
-#include "labs-0x01/chapter02.h"
+#include "labs_0x01/labs_0x01.h"
 
 using namespace std;
 
-void chapter02 () {
-    cout << "Tasks from chapter 2.\n";
-    cout << "Enter radius:  ";
-    double radius = 0;
-    double pi = 3.1415;
-    cin >> radius;
-    cout << "Area of circle with radius " << radius << " = " << pi*radius*radius << endl;
-    fstream textFile;
-    string text;
-    textFile.open("files/chapter-02.txt");
-    if (textFile.is_open()) {
-        std::getline(textFile,text,'\0');
-        cout << "Textfile: \n" << text << endl;
-        int index = 0, spaceCounter = 0;
-        while ((index = text.find(' ',index)) != -1) { spaceCounter++; index++; }
-        cout << "Spaces in text " << spaceCounter << endl;
-    } else cout << "Error open file: files/chapther-02.txt" << endl;
-    textFile.close();
-}
-
 int main(int argc, char** argv)
 {
-    Chapter02 t;
-    chapter02();
+    Labs_0x01();
     return 0;
 }
 
