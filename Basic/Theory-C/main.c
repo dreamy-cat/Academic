f8c58855bac36b7ff8144b40a6e0e43e55be86a1
Alexander Serov <alexander.serov@protonmail.com>
2022-06-02 18:38:34 +0300

    Add 13 chapter. File IO. Update Theory-C as project to stream.

---
+++ Basic/Theory-C/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/*  Добавить комментарий относительно конфигурации компиляции и сборки.
+ *
+ * */
+
+void theory_03(void);
+void theory_04(void);
+void theory_13(void);
+
+int main()
+{
+    printf("Theory for classic C and simple algorithms.\n");
+    /*
+    theory_03();
+    theory_04();
+    */
+    theory_13();
+
+    printf("Some extra tasks using classic structures and data.\n");
+
+    // stack_and_structures();
+    // queue_and_array();
+
+    return 0;
+}
